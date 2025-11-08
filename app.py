816462223a5b724666a0ddf4b79507399b152f87
Ramyaeedara06 <eedararamya6@gmail.com>
2025-11-08 11:54:39 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "🚀 Successfully deployed the app via Jenkins + ArgoCD!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
